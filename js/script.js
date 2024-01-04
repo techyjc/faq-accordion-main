1ba811c8d9625c3ae650c5ab2f75d97dcf9458fc
techyjc <85077790+techyjc@users.noreply.github.com>
2024-01-04 19:48:44 +0000

    Question as button Trigger

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 const actionbtn = document.querySelectorAll('.action-btn');
+const questionbtn = document.querySelectorAll('.faq-question');
 const faqans = document.querySelectorAll('.faq-answer');
 
 close_accordion()
 
-actionbtn.forEach(btn => {
+questionbtn.forEach(qbtn => {
+    qbtn.addEventListener('click', (e) => {
+        var qst_target = e.target.getAttribute("aria-controls");
+        var qst_element = document.getElementById(qst_target);
+        var btn_target = e.target;    
+        close_accordion();
+        qst_element.setAttribute('aria-expanded', 'true');
+        toggle_btnicon(btn_target.nextElementSibling,qst_element.getAttribute('aria-expanded'))
+    });
+})
 
+actionbtn.forEach(btn => {
     btn.addEventListener('click', (e) => {
         var ans_target = e.target.getAttribute("aria-controls");
         var ans_element = document.getElementById(ans_target);
